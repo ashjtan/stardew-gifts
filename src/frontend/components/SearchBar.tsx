0f79e0f83385f1a7076745f8c352bf39ecba2240
Ashley Tan <tan.ashley32@gmail.com>
2025-04-23 15:42:41 -0700

    SearchBar calls autocomplete api

--- src/frontend/components/SearchBar.tsx
+++ src/frontend/components/SearchBar.tsx
@@ -1,9 +1,26 @@
+import axios from 'axios';
+
 const SEARCH_PLACEHOLDER = 'Item, villager';
+
+async function getAutocompleteSuggestions(event: React.ChangeEvent<HTMLInputElement>) {
+	const value = event.target.value;
+	try {
+		const response = await axios.get(`http://localhost:3000/autocomplete?q=${value}`);
+		console.log(response.data);
+	} catch (error) {
+		console.error('Error fetching autocomplete suggestions:', error);
+	}
+}
 
 export default function SearchBar() {
 	return (
 		<form className="container-fluid">
-			<input type="text" placeholder={SEARCH_PLACEHOLDER} />
+			<input
+				type="search"
+				aria-label={SEARCH_PLACEHOLDER}
+				placeholder={SEARCH_PLACEHOLDER}
+				onChange={(e) => getAutocompleteSuggestions(e)}
+			/>
 			<button type="submit">Search</button>
 		</form>
 	);
