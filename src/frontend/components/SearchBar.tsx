39eb3923b4e990330e4e527dc42789f54be32ca3
Ashley Tan <tan.ashley32@gmail.com>
2025-04-24 13:59:58 -0700

    init display suggestions

--- src/frontend/components/SearchBar.tsx
+++ src/frontend/components/SearchBar.tsx
@@ -1,27 +1,55 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 const SEARCH_PLACEHOLDER = 'Item, villager';
 
-async function getAutocompleteSuggestions(event: React.ChangeEvent<HTMLInputElement>) {
-	const value = event.target.value;
-	try {
-		const response = await axios.get(`http://localhost:3000/autocomplete?q=${value}`);
-		console.log(response.data);
-	} catch (error) {
-		console.error('Error fetching autocomplete suggestions:', error);
-	}
+interface AutocompleteSuggestionsProps {
+	suggestions: string[];
+}
+
+function AutocompleteSuggestions({ suggestions }: AutocompleteSuggestionsProps) {
+	const listItems = suggestions.map((suggestion) => (
+		<li>
+			<a href="#">{suggestion}</a>
+		</li>
+	));
+
+	return (
+		<details className="dropdown">
+			<summary></summary>
+			<ul dir="rtl">{listItems}</ul>
+		</details>
+	);
 }
 
 export default function SearchBar() {
+	const [suggestions, setSuggestions] = useState<string[]>([]);
+
+	// TODO: throttle/debounce, cache, especially for backspacing
+	async function fetchAutocompleteSuggestions(query: string) {
+		try {
+			const response = await axios.get(`http://localhost:3000/autocomplete?q=${query}`);
+			console.log(response.data);
+			setSuggestions(response.data);
+		} catch (error) {
+			console.error('Error fetching autocomplete suggestions:', error);
+		}
+	}
+
 	return (
-		<form className="container-fluid">
+		<form autoComplete="off" className="container-fluid">
 			<input
 				type="search"
+				list=""
 				aria-label={SEARCH_PLACEHOLDER}
 				placeholder={SEARCH_PLACEHOLDER}
-				onChange={(e) => getAutocompleteSuggestions(e)}
+				onChange={(e) => fetchAutocompleteSuggestions(e.target.value)}
 			/>
 			<button type="submit">Search</button>
+
+			<div>
+				<AutocompleteSuggestions suggestions={suggestions} />
+			</div>
 		</form>
 	);
 }
